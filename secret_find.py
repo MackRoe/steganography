30c3c1ad2a45307d2fcc34c2239e96d165ca9ea6
MackRoe <elaine.music@students.makeschool.com>
2020-04-29 08:55:25 -0700

    refactor, now produces an all black image

--- secret_find.py
+++ secret_find.py
@@ -1,11 +1,11 @@
 # python
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 
 
 def decode_image(file_location):
     encoded_image = Image.open(file_location)
     red_channel = encoded_image.split()[0]
-    rgb_rc = red_channel.convert('RGB')
+    # rgb_rc = red_channel.convert('RGB')
 
     x_size = encoded_image.size[0]
     y_size = encoded_image.size[1]
@@ -17,26 +17,30 @@ def decode_image(file_location):
     # -----------------------------------
     # loop through all pixels in encoded_image
     for x in range(x_size):
-        print(x)
+        # print(x)
         for y in range(y_size):
-            print(y)
+            # print(y)
             # xy = int(str(x) + str(y))
             # print('xy', xy)
-            r,g,b = rgb_rc.getpixel((x, y))  # #ERROR!
-            print('RGB', r,g,b)
-            print('r', r)
+            # r,g,b = rgb_rc.getpixel((x, y))
+            # print('RGB', r,g,b)
+            # print('r', r)
             # check LSB
-            binary_r = bin(r)
-            length_binary = len(str(r))
-            print(length_binary)
-            if binary_r[length_binary-1] == 1:
-                r,g,b = 255, 255, 255
-            # check LSB
-            elif binary_r[length_binary-1] == 0:
-                r,g,b = 0, 0, 0
-            decoded_image.putpixel((x,y), (r,g,b))
+            binary_r = bin(red_channel.getpixel((x, y)))
+            # length_binary = len(str(r))
+            # print(length_binary)
+
+            # check if lsb of red_channel is 1
+            if binary_r[-1] == 0:
+                # make pixel black if LSB is 0
+                pixels[x, y] = (255, 255, 255)
+            else:
+                # make pixel white
+                pixels[x, y] = (0, 0, 0)
+
+            # decoded_image.putpixel((x,y), (r,g,b))
 
 
-    decoded_image.save("decoded_image.png")
+    decoded_image.save("decoded_image3.png")
 
 print(decode_image("encoded_sample.png"))
